83defb5e3f1523e1b1a984cd10cc8c34f08c9a0e
stdefeber <st.de.feber@gmail.com>
2024-08-14 11:42:51 +0200

    Update test_lb_bridge.py

--- tests/hdl/test_lb_bridge.py
+++ tests/hdl/test_lb_bridge.py
@@ -5,7 +5,7 @@
 """
 
 import sys
-from attrs.setters import convert
+#from attrs.setters import convert
 sys.path.insert(0, '../..')
 import pytest
 from sim import Simulator, CliArgs, path_join, parent_dir
